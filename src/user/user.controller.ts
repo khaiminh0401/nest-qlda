0c766d1ab383ec2a062b0daabd34f5b97db2ea93
dkminh.hcm <dkminh.hcm@vnpt.vn>
2024-09-01 01:10:28 +0700

    add prefix vs api/user/getInfo

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,17 +1,24 @@
-import { Get, Controller, UseGuards } from "@nestjs/common";
-import { AuthGuard } from "src/auth/auth.guard";
-import { UserModel } from "src/model/user.model";
-import { PrismaService } from "src/service/prisma.service";
+import { Get, Controller, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/auth/auth.guard';
+import { UserModel } from 'src/model/user.model';
+import { PrismaService } from 'src/service/prisma.service';
 
-@Controller("/user")
-export class UserController{
-    constructor(
-        private prismaSerivce: PrismaService
-    ) {}
-    @UseGuards(AuthGuard)
-    @Get("/")
-    async getUser(): Promise<UserModel[] | null> {
-      return await this.prismaSerivce.user.findMany();
-    }
+@Controller('/user')
+export class UserController {
+  constructor(private prismaSerivce: PrismaService) {}
+  @UseGuards(AuthGuard)
+  @Get('/')
+  async getUser(): Promise<UserModel[] | null> {
+    return await this.prismaSerivce.user.findMany();
+  }
 
+  @UseGuards(AuthGuard)
+  @Get('/getInfo')
+  async getInfo(username: string): Promise<any> {
+    return await this.prismaSerivce.user.findFirst({
+      where: {
+        username: username,
+      },
+    });
+  }
 }
