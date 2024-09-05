41a6f8b47c0c23fcbddb7b8b73c0dbe872889153
dkminh.hcm <dkminh.hcm@vnpt.vn>
2024-09-05 23:25:42 +0700

    add error

--- src/interceptor/transform.interceptor.ts
+++ src/interceptor/transform.interceptor.ts
@@ -1,15 +1,35 @@
-import { Injectable, NestInterceptor, ExecutionContext, CallHandler } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+  BadGatewayException,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 export interface Response<T> {
   data: T;
-  status: 0 | 1;
+  status: number;
 }
 
 @Injectable()
-export class TransformInterceptor<T> implements NestInterceptor<T, Response<T>> {
-  intercept(context: ExecutionContext, next: CallHandler): Observable<Response<T>> {
-    return next.handle().pipe(map(data => ({ data: data, status: 1 })));
+export class TransformInterceptor<T>
+  implements NestInterceptor<T, Response<T>>
+{
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler,
+  ): Observable<Response<T>> {
+    return next.handle().pipe(
+      map((data) => ({ data: data, status: 1 })),
+      catchError((err) => {
+        // Catch the error and throw a new error or log the error
+        console.error('Error in TransformInterceptor:', err);
+        
+        // Throw a new error or pass the error through
+        return throwError(() => new BadGatewayException('Something went wrong!'));
+      }),
+    );
   }
 }
